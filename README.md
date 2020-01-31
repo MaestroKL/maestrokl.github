98f12128e1815a40bf96cd035ba6effadbb91cb8
Kevin Lai <maestrokl@Kevins-Mac-mini.fios-router.home>
2020-01-31 01:51:24 -0500

    connected AJAX without error messages although it doesn't console.log anything

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 1. Link HTML/CSS/JS/JQUERY. Start readme.txt
 2. Create free GitHub repo and link to local
 3. Project approval with Paul
+
+01/30/2020:
+1. get KBB API
+2. Obtain key. successfully have working URL
--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log ("test")
+(()=>{
+
+    $.ajax({
+        url: "https://api.sportsdata.io/v3/nfl/scores/xml/TeamSeasonStats/REG2019?key=e901378c0864450f84cb2993bd089fa7",
+        success: function(data){
+            console.log(data)
+        }, 
+        error: ()=>{
+            console.log("bad")
+        }
+    })
+
+})
+
+    // https://api.sportsdata.io/v3/nfl/scores/xml/TeamSeasonStats/REG2019
+
+    // Ocp-Apim-Subscription-Key: {key}
+        // not sure what this is but it was labelled:
+        // "This is the documentation for SportsDataIO's NFL API. All of our API endpoints can be accessed via an HTTP GET request using your API key. The API key can be passed either as a query parameter or using the following HTTP request header.
+        // Ocp-Apim-Subscription-Key: {key} "//
+
+
+    // API Key: e901378c0864450f84cb2993bd089fa7
+    // Login: leikevy@//gaproject1
+
+
+
+
